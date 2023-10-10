1cd4a53f9d2728fc42feeb06f256cd904515a0a9
jochemwhite <j.vanderwit@amrio.nl>
2023-10-10 15:57:25 +0200

    updated services

--- src/grpc/Action.ts
+++ src/grpc/Action.ts
@@ -2,8 +2,8 @@ import * as grpc from "@grpc/grpc-js";
 import { ActionServiceClient } from "../proto/Actions";
 import * as config from "../config.json";
 
-const host = config.checkAction.host;
-const port = config.checkAction.port;
+const host = config.checkAction.dns;
+const port = config.checkAction.dns;
 const connectionString = `${host}:${port}`;
 
 const Actions = new ActionServiceClient(connectionString, grpc.credentials.createInsecure());
--- src/grpc/IRC.ts
+++ src/grpc/IRC.ts
@@ -2,7 +2,7 @@ import * as grpc from "@grpc/grpc-js";
 import { ChatServiceClient } from "../proto/IRC";
 import * as config from "../config.json";
 
-const host = config.irc.host;
+const host = config.irc.dns;
 const port = config.irc.port;
 const connectionString = `${host}:${port}`;
 
