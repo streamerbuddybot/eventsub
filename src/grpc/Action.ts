1a0c618f8afe0eb88126e2d6a1b93d3a194936ad
jochemwhite <j.vanderwit@amrio.nl>
2023-10-10 16:02:08 +0200

    test

--- src/grpc/Action.ts
+++ src/grpc/Action.ts
@@ -3,7 +3,7 @@ import { ActionServiceClient } from "../proto/Actions";
 import * as config from "../config.json";
 
 const host = config.checkAction.dns;
-const port = config.checkAction.dns;
+const port = config.checkAction.port;
 const connectionString = `${host}:${port}`;
 
 const Actions = new ActionServiceClient(connectionString, grpc.credentials.createInsecure());
